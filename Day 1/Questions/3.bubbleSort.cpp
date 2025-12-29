7eae77c62766924bbd8c8991d4b2a0d39f66ed1b
Viraj Dayle <virajdayle891@gmail.com>
2025-12-29 23:09:31 +0530

    optimise bubble sort

--- Day 1/Questions/3.bubbleSort.cpp
+++ Day 1/Questions/3.bubbleSort.cpp
@@ -6,10 +6,18 @@ void bubbleSort(vector<int> &arr)
     int n = arr.size();
     for (int i = n - 1; i >= 1; i--)
     {
+        int didSwap = 0;
         for (int j = 0; j < i; j++)
         {
             if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0)
+        {
+            break;
         }
     }
     return;
